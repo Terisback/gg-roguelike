7d2ea5db45aba95576b29ed3f155ce956cb9912c
Anton Terisback <terisbackno@gmail.com>
2021-05-13 13:08:07 +0500

    Initial commit

---
+++ DungeonMaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DungeonMaster
+{
+    class DungeonMaster
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
